2e21703d1cfa3a37f8312b50751f7e31c545fc74
radiophysiker <bolshakovdmitry@bk.ru>
2024-11-22 15:16:57 +0300

    add ecosystem.config.js

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/radiophysiker/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp .env.production ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
+      'post-deploy': 'export PATH=$PATH:~/.nvm/versions/node/v22.11.0/bin && cd backend && npm i && npm run build && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
